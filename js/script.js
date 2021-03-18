a1ebf0f00b3059575ef9b610a533cbdce982467d
Alessandro Marega <ale_marega@hotmail.it>
2021-03-18 18:42:23 +0100

    agginti effetti in js di apparizione lista e list-items

--- js/script.js
+++ js/script.js
@@ -14,8 +14,14 @@ $("#list_element").keydown(function (e) {
       // immetto il valore nel mio list-item in template
       listItemTemplate.prepend(inputValue);
 
+      // faccio apparire la lista
+      $(".todo_list").slideDown(1000);
+
       // innietto tutto il list-item nella lista visibile in html
       $(".todo_list").append(listItemTemplate);
+
+      // faccio apparire i list items
+      listItemTemplate.slideDown(1000);
 
       // setto a vuoto il valore della casella di input
       $(this).val("");
@@ -24,6 +30,6 @@ $("#list_element").keydown(function (e) {
 });
 
 // creo un evento on-click sul bottone che elimini l'elemento e la riga corrispondente
-$(".todo_list").on("click", $("#btn"), function () {
-  $("#btn").parent().remove();
+$(".todo_list").on("click", "#btn", function () {
+  $(this).parent().slideUp('slow');
 });
