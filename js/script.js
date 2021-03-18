878e2ba055b7f86d463596212611eb93aac7736b
Alessandro Marega <ale_marega@hotmail.it>
2021-03-18 16:12:02 +0100

    aggiunto evento alla prresione su tasto enter che inserisce valore nella lista

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // creare una todo list che permetta di aggiungere (tramite campo input) e rimuovere gli elementi!
+
+// clono il template
+var listItemTemplate = $(".template .todo_list_item").clone();
+
+// creo un evento alla pressione sul tasto invio
+$("#list_element").keydown(function (e) {
+  if (e.which == 13) {
+    // prendo il valore che arriva dall'input
+    var inputValue = $(this).val();
+    listItemTemplate.text(inputValue);
+    $(".todo_list").append(listItemTemplate);
+  }
+});
